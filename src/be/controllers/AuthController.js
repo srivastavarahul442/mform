2b3fd74cae94407be9b082f7691919fcaa285314
srivastavarahul442 <srivastavarahul442@gmail.com>
2026-06-12 17:47:01 +0530

    refactored files

--- src/be/controllers/AuthController.js
+++
@@ -1,6 +0,0 @@
-// AuthController.js
-// Handles auth-related routes that are not covered by LoginController,
-// MeController, or RegisterCompanyController.
-// Example: logout, refresh token, change password.
-
-// TODO: implement as needed
